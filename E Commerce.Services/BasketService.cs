f2a035e035d72e5e7c41c1ec0540f0fd486c924a
nonoriz <nouryahmed607@gmail.com>
2025-11-13 16:01:43 +0200

    C44-G03-API04

---
+++ E Commerce.Services/BasketService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using E_Commerce.Domain.Contracts;
+using E_Commerce.Domain.Entities.BasketModule;
+using E_Commerce.Services_Abstraction;
+using E_Commerce.Shared.DTOs.BasketDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly IBasketRepository basketRepository;
+        private readonly IMapper mapper;
+
+        public BasketService(IBasketRepository basketRepository,IMapper mapper)
+        {
+            this.basketRepository = basketRepository;
+            this.mapper = mapper;
+        }
+        public async Task<BasketDTO> CreateOrUpdateBasketAsync(BasketDTO basket)
+        {
+            var CustomerBasket=mapper.Map<BasketDTO,CustomerBasket>(basket);
+            var CreatedOrUpdatedBasket= await basketRepository.CreateOrUpdateBasketAsync(CustomerBasket);
+            return mapper.Map<CustomerBasket,BasketDTO>(CreatedOrUpdatedBasket);
+        }
+
+        public async Task<bool> DeleteBasketAsync(string id)=>await basketRepository.DeleteBasketAsync(id);
+
+
+        public async Task<BasketDTO> GetBasketAsync(string id)
+        {
+           var Basket=await basketRepository.GetBasketAsync(id);
+            return  mapper.Map<CustomerBasket,BasketDTO>(Basket!);
+        }
+    }
+}
